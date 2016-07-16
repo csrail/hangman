178ea0525f15595d59ed91dec2b7c6b8579f3e73
Cornelius <cornelius.soon@gmail.com>
2016-07-17 09:16:34 +1200

    Create class SecretWord to encapsulate codeblocks

--- hangman.rb
+++ hangman.rb
@@ -1,19 +1,48 @@
-#Store path to variable name
-dictionary = File.open("./wordlist.txt")
-# p dictionary #=>  #File:./wordlist.txt
 
-wordlist = []
+class SecretWord
+	#attr_reader :dictionary, :wordlist
+	attr_accessor :dictionary, :wordlist
 
-def adjust_word
-	self.strip.downcase
+	def initialize
+		#Assign path to variable name "dictionary"
+		# p dictionary #=>  #File:./wordlist.txt
+		@dictionary = File.open("./wordlist.txt")
+		@wordlist = []
+
+		generate_word_bank
+	end
+
+
+	public
+
+	def random_word
+		wordlist[random_index]
+	end
+
+
+	protected
+
+	def adjust_word
+		self.strip.downcase
+	end
+
+	def total_number_of_words
+		wordlist.length
+	end
+
+	def random_index
+		rand(total_number_of_words)
+	end
+
+	def generate_word_bank
+		dictionary.readlines.each do |line|
+			wordlist << line.strip if line.strip.downcase.length >= 5 && line.strip.downcase.length <= 12
+		end		
+	end
+
+
+
 end
 
-dictionary.readlines.each do |line|
-	wordlist << line.strip if line.strip.downcase.length >= 5 && line.strip.downcase.length <= 12
-end
-
-total_number_of_words = wordlist.length
-
-random_index = rand(total_number_of_words)
-
-p wordlist[random_index]
+secret_word = SecretWord.new
+p secret_word.random_word
