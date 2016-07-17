e409670ad98227692721b76d094fa8f446d997d9
Cornelius <cornelius.soon@gmail.com>
2016-07-17 21:02:26 +1200

    Add class WordBank separate from class SecretWord

--- hangman.rb
+++ hangman.rb
@@ -1,7 +1,7 @@
+timer_start = Time.now
 
-class SecretWord
-	#attr_reader :dictionary, :wordlist
-	attr_accessor :dictionary, :wordlist
+class WordBank
+	attr_reader :dictionary, :wordlist, :find_random_word, :close_stream
 
 	def initialize
 		#Assign path to variable name "dictionary"
@@ -9,40 +9,59 @@ class SecretWord
 		@dictionary = File.open("./wordlist.txt")
 		@wordlist = []
 
-		generate_word_bank
+		create_word_bank
+		
+		@find_random_word = wordlist[random_index] 
 	end
 
-
-	public
-
-	def random_word
-		wordlist[random_index]
-	end
-
+	
 
 	protected
 
-	def adjust_word
-		self.strip.downcase
+	def random_index
+		rand(total_number_of_words)
 	end
 
 	def total_number_of_words
 		wordlist.length
 	end
 
-	def random_index
-		rand(total_number_of_words)
-	end
-
-	def generate_word_bank
+	def create_word_bank
 		dictionary.readlines.each do |line|
-			wordlist << line.strip if line.strip.downcase.length >= 5 && line.strip.downcase.length <= 12
+			wordlist << line.strip.downcase if line.strip.length >= 5 && line.strip.length <= 12
 		end		
 	end
 
+	def close_dictionary
+		dictionary.close
+	end
 
+end
+
+
+class SecretWord
+	attr_reader :wordbank
+
+	def initialize
+		@wordbank = access_wordbank
+	end
+
+	def access_wordbank
+		WordBank.new
+	end
+
+	def generate_random_word
+		wordbank.find_random_word
+	end
+
+	def close_wordbank
+		wordbank.dictionary.close
+	end
 
 end
 
 secret_word = SecretWord.new
-p secret_word.random_word
+p secret_word.generate_random_word
+secret_word.close_wordbank
+
+puts "Elapsed Time: #{(Time.now - timer_start)*1000} milliseconds"
