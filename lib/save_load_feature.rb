0bb3c8cc8b428d0f6074ad27ab3a9cfb4370a2b3
Cornelius <cornelius.soon@gmail.com>
2016-07-21 12:37:53 +1200

    Fix to_json method

--- lib/save_load_feature.rb
+++ lib/save_load_feature.rb
@@ -1,26 +1,4 @@
 module SaveFeature
-	def to_json
-		{
-			@secret_word => secret_word,
-			@display_letters => display_letters,
-			@player_guess => player_guess,
-			@incorrect_guess_history => incorrect_guess_history,
-			@guesses_left => guesses_left,
-
-			@user_letter_display => user_letter_display,
-			@user_incorrect_guess_history => user_incorrect_guess_history,
-
-			@player_wins => player_wins,
-			@player_loses => player_loses,
-		}.to_json
-	end
-
-	def commit_save(save)
-		file_path = File.open("./lib/save-states/hello_world.json", "w") do |f|
-			f.write(save)
-		end
-	end
-
 
 	def save?
 		if player_guess == "save"
@@ -29,11 +7,26 @@ module SaveFeature
 			commit_save(save_state)
 			abort
 		end
-	end
+	end	
 
-	def save_state
+	def save_state #is now a json string
 		self.to_json
 	end	
+
+	def commit_save(save)
+		file_path = File.open("./lib/save-states/brute_force_2.json", "w") do |f|
+			f.write(save)
+		end
+	end
+
+	def to_json
+		hash = {}
+		self.instance_variables.each do |var|
+			hash[var] = self.instance_variable_get var
+		end
+		hash.to_json
+	end
+
 end
 
 module LoadFeature
@@ -41,3 +34,5 @@ module LoadFeature
 		
 	end
 end
+
+
